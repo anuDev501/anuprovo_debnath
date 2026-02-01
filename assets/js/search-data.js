f1f16f61d583737427cd39c78193826ce7a83586
anuDev501 <166212030+anuDev501@users.noreply.github.com>
2026-02-01 10:58:59 +0000

    Deploying to gh-pages from @ anuDev501/anuprovo_debnath@bd347b59b904638d4d929126d4ecdead6672d598 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/anuprovo_debnath/cv/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/anuprovo_debnath/people/";
-          },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
