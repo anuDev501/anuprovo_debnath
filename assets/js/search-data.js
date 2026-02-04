69381c83f03275af8c32e64149af0551dea5895b
anuprovo-debnath <166212030+anuprovo-debnath@users.noreply.github.com>
2026-02-04 20:16:32 +0000

    Deploying to gh-pages from @ anuprovo-debnath/anuprovo-debnath.github.io@139a6844e59db843bc735315035514a58b78fb40 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,12 +44,7 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "books-1984",
+            },{id: "books-1984",
           title: '1984',
           description: "",
           section: "Books",handler: () => {
